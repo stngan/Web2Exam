40231fe51d10504c1f6ca3c610885127be58c334
Tuyet Ngan Ngo Song <ngannst20406c@st.uel.edu.vn>
2023-02-13 13:33:00 +0700

    chốt binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { HeaderComponent } from './header/header.component';
+// import { SidebarComponent } from './sidebar/sidebar.component';
+// import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    HeaderComponent,
+    // SidebarComponent,
+    // HeaderComponent,
     ContentComponent
   ],
   imports: [
