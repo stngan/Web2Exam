9fb603880dd84e9603d775778c3c996d3ea02f75
Tuyet Ngan Ngo Song <ngannst20406c@st.uel.edu.vn>
2023-02-13 13:29:09 +0700

    2 way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component'; 
+import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { ContentComponent } from './content/content.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
     <h2>Product: {{productName}}</h2>
 
     <h2>{{"Product Price: " + productPrice}}</h2>
-    
+
     <h2>{{productCategory.toUpperCase()}}</h2>
 
     <h3>{{ test() }}</h3>
@@ -19,10 +19,39 @@ import { Component } from '@angular/core';
 
     </button>
 
-    
+    <hr>
+
+    <!-- làm form với 2 way binding -->
+    <div>
+      <label for="">Enter student 1: </label>
+      <input type="text" [(ngModel)] = "st1">
+
+      <label for="">Enter student 2: </label>
+      <input type="text" [(ngModel)] = "st2">
+
+      <label for="">Enter student 3: </label>
+      <input type="text" [(ngModel)] = "st3">
+
+
+      <!-- <button (click)="saveData()">
+        Save data
+      </button> -->
+
+      <button (click)="resetData()">
+        Reset data
+      </button>
+
+      <p>Full Name student 1: <span>{{st1}}</span></p>
+      <p>Full Name student 2: <span>{{st2}}</span></p>
+      <p>Full Name student 3: <span>{{st3}}</span></p>
+    </div>
+
+
   `
 })
 export class ContentComponent {
+
+
   productName = "TITLE BOOK";
   productPrice = 5.6;
   productCategory = "Hàng tiêu dùng";
@@ -35,6 +64,16 @@ export class ContentComponent {
 
   test() {
     console.log("Hello");
+  }
+
+  st1 ="";
+  st2 ="";
+  st3 ="";
+
+  resetData() {
+    this.st1 = "";
+    this.st2 = "";
+    this.st3 = "";
   }
 
 }
